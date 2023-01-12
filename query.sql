384af3b41feab89725f10725660546b59542ff5d
Gonzalo Loyola <34005904+AlFcl@users.noreply.github.com>
2023-01-12 12:22:17 -0300

    Update query.sql

--- query.sql
+++ query.sql
@@ -8,3 +8,18 @@ WHERE cl.status = 'Active'
 GROUP by f.userid
 ORDER BY total DESC
 ## query by ALFcl 
+
+
+####  OHER QUERY  FOR GET ALL FILED BY  CLIENT ID
+
+SELECT 
+c.id as IdClient,f.filename as NameFile, f.dateadded AS DateAdded
+FROM
+tblclientsfiles f
+INNER JOIN
+tblclients
+c
+ON f.userid = c.id
+WHERE c.status = 'Active'
+
+## query by ALFcl..
